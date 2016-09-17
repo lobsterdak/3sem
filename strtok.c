c4f153271a03c9a6c4f66a7202efe9c62932deae
VladMiryaha <vlad.miryaha@gmail.com>
2016-09-17 18:03:28 +0300

    ok

--- strtok.c
+++ strtok.c
@@ -4,11 +4,21 @@
 
 void Split(char* string, char * delimiters, char*** tokens, int* tokensCount);
 
+/*
+ * FIXIT: Никаких магических чисел
+ * Создайте константы: 
+ * #define MAX_STRING_SIZE 50
+ * #define MAX_DELIMETERS_COUNT 50
+ * и т.д.
+ * 
+ * P.S. c освободжением памяти мы с вами на семинаре разобрались 
+ */
+
 int main () 
 {	
 	int i, tokensCount;
 	char string[50], delimiters[50];
-	char** tokens = (char**)malloc(sizeof(char) * 50); // количество слов 50, длина слова 10 символов
+	char** tokens = (char**)malloc(sizeof(char*) * 50); // количество слов 50, длина слова 10 символов
 	for (i = 0; i < 50; i++)
 	{
 		tokens[i] = (char*)malloc(sizeof(char) * 10);
